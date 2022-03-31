fecaf1179f934fe42f5bff738d94bc303408de6e
Abyou <emandaabyou@gmail.com>
2022-03-31 17:35:12 -0500

    Adding some functions for getting the required elements.

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+// get required the elements that enable us to switch and interface the pages
+
+const quizStartBtn = document.querySelector(".quiz-start-btn button");
+const quizInfoContainer = document.querySelector(".quiz-info-container");
+const exitBtn =
+  quiz - info - container.document.querySelector(".buttons #exit");
+const continueBtn =
+  quiz - info - container.document.querySelector(".buttons #continue");
+
+// Add functionality to the start button when it is clicked
+quizStartBtn.onclick = function () {
+  quiz - info - container.add("activeInfo");
+};
